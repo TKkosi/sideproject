a3dc96d6045486113a33824087f5a3c0280f24c3
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-08-10 08:23:04 +0100

    some stuff

--- main.js
+++ main.js
@@ -74,3 +74,25 @@ switch(randomNumber){
         console.log('Outlook not so good');
         break;  
 }
+
+Race Day
+
+let raceNumber = Math.floor(Math.random() * 1000);
+let early = true ;
+let age = 21 ;
+
+if(early && age > 18){
+    raceNumber += 1000;
+}
+
+if(early && age > 18){
+    console.log(`Race will begin at 9:30 , your race number is ${raceNumber}.`)
+}
+else if(!early && age > 18){
+    console.log(`Race will begin at 11:00 , your race number is : ${raceNumber}.`)}
+else if(age < 18){
+    console.log(`Race will begin at 12:30, your race number is : ${raceNumber}`)
+}
+else{
+    console.log(`Please approach the registration desk , thanks!`)
+}
